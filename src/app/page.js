b8a27bc90b0e13e6b268d7820870edb84d035796
Tuguldur Tuvshintugs <146443087+tuguldur0@users.noreply.github.com>
2026-08-19 12:37:24 +0800

    jn

--- src/app/page.js
+++ src/app/page.js
@@ -1,69 +1,116 @@
-import Image from "next/image";
+"use client"
+
+import Link from "next/link";
+import { useEffect, useState } from "react"
+import { supabase } from "../../lib/supabase/client";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.js
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+            if(!loading) return;
+            const stupidAsyncFunc = async () => {
+              const { data, error } = await supabase.from('products').select('*');
+              if(error) console.log(error);
+              else setProducts(data);
+            }
+            stupidAsyncFunc();
+         }, [])
+    
+    const [copyoproducts, setCopyoproducts] = useState(products);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [first, setFirst] = useState(0);
+    const [last, setLast] = useState(28);
+    const [count, setCount] = useState(1);
+    const [maxCount, setMaxCount] = useState(7)
+    
+    const search = (term) => {
+        setCopyoproducts(products.filter((product) => product.title.toLowerCase().includes(term)))
+        setFirst(0);
+        setLast(28);
+        setMaxCount(Math.floor(products.filter((product) => product.title.toLowerCase().includes(term)).length)/28)
+    }
+    const forward = () => {
+        if(count < maxCount){
+        setFirst(first+28);
+        setLast(last+28);
+        setCount(count+1);
+        }
+    }
+    const backward = () => {
+        if(first != 0 && last != 28){    
+        setFirst(first-28);
+        setLast(last-28);
+        setCount(count-1)
+    }
+    }
+    const filter = (term) => {
+        if(term == "all"){
+            setCopyoproducts(products)
+            setFirst(0);
+            setLast(28); 
+            setCount(1)
+            setMaxCount(Math.ceil(products.length/28))
+        } else if(term == "50"){
+            setCopyoproducts(products.filter((product) => product.price<50))
+            setFirst(0);
+            setLast(28); 
+            setCount(1)
+            setMaxCount(Math.ceil(products.filter((product) => product.price<50).length/28))
+        } else if(term == "50-200"){
+            setCopyoproducts(products.filter((product) => product.price>50 && product.price < 200))
+            setFirst(0);
+            setLast(28);
+            setCount(1)
+            setMaxCount(Math.ceil(products.filter((product) => product.price > 50 && product.price < 200).length/28))
+        } else if(term == "200"){
+            setCopyoproducts(products.filter((product) => product.price>200))
+            setFirst(0);
+            setLast(28);
+            setCount(1) 
+            setMaxCount(Math.ceil(products.filter((product) => product.price > 200).length/28))
+
+        }
+    }
+
+    useEffect(() => {
+        filter("all")
+    }, [products])
+    if (!products) return <div>Loading...</div>;
+
+    return (
+        <div className="flex flex-col gap-6 items-center">
+            <div className="h-7 flex justify-between">
+                <div className="flex gap-1">
+                    <button onClick={() => filter("all")} className="w-24 h-fits border-2 border-gray-300 rounded-md flex justify-center align-center">All</button>
+                    <button onClick={() => filter("50")} className="w-24 h-fits border-2 border-gray-300 rounded-md flex justify-center align-center">Under 50$</button>
+                    <button onClick={() => filter("50-200")} className="w-24 h-fits border-2 border-gray-300 rounded-md flex justify-center align-center">50-200$</button>
+                    <button onClick={() => filter("200")} className="w-24 h-fits border-2 border-gray-300 rounded-md flex justify-center align-center">Over 200$</button>
+                </div>
+                <div className="border-2 border-gray-200 rounded-md px-1">
+                    <input onChange={(event) => {setSearchTerm(event.target.value)}} value={searchTerm} placeholder="Search"></input>
+                    <button onClick={() => search(searchTerm.toLowerCase())}>🔍</button>
+                </div>
+            </div>
+            <div className="grid grid-cols-4 gap-2">
+            {products.map((product) => {
+                return (
+                  <Link href={`/products/${product.id}`} key={product.id} className="w-fit p-4 bg-gray-50 rounded-xl border border-gray-400">
+                        <p>{product.title}</p>
+                        <p>Price: {product.price}$</p>
+                        <p>Brand: {product.brand}</p>
+                        <p>Category: {product.category}</p>
+                        <p>Stock: {product.stock}</p>
+                        <img src={product.thumbnail}></img>
+                  </Link>
+                )
+            })}
+            </div>
+            <div className="flex justify-between w-12">
+                <button onClick={backward}>{'<'}</button>
+                <p>{count}/{maxCount}</p>
+                <button onClick={forward}>{'>'}</button>  
+            </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    )
 }
---
+++ src/app/products/[id]/page.js
@@ -0,0 +1,40 @@
+"use client"
+
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { supabase } from "../../../../lib/supabase/client";
+
+export default function Home() {
+    const { id } = useParams();
+    const [item, setItem] = useState(null);
+    const [loading, setLoading] = useState(true)
+    
+    useEffect(() => {
+        if(!loading) return;
+        const oeoerg = async () => {
+            const { data, error } = await supabase.from('products').select('*').eq("id", id).single();
+            if(error) console.log(error);
+            else setItem(data);
+        }
+        oeoerg();
+     }, [])
+    if (!item) return <div>Loading...</div>;
+
+    return (
+        <div className="h-screen w-screen flex flex-col items-center justify-center">
+            <div className="border-2 border-gray-200 rounded-md flex gap-4 overflow-hidden">
+                <img src={item.thumbnail}></img>
+                <div className="flex flex-col gap-1">
+                    <p className="text-2xl">{item.title}</p>
+                    <p>
+                    Description:
+                    <br></br>
+                    {item.description}
+                    </p>
+                    <p>Rating: {item.rating}</p>
+                    <p>Stock: {item.stock}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
